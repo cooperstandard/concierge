b8fc6d1c9a407938f1dd75c79856a5d6b745988d
Cooper <cooper@cooperstandard.org>
2023-01-12 21:53:25 -0700

    security

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 const router = express.Router()
 const Recipe = require('../models/recipe');
+const User = require('../models/user');
 
 
 // Get all recipes
@@ -12,6 +13,17 @@ router.get('/all', async (req, res) => {
     try{
         
         const data = await Recipe.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.get('/users', async (req, res) => {
+    try{
+        
+        const data = await User.find();
         res.json(data)
     }
     catch(error){
@@ -33,15 +45,34 @@ router.get('/search', async (req, res) => {
 })
 
 //Post Method
-router.post('/post', async (req, res) => {
+router.post('/recipe', async (req, res) => {
     const data = new Recipe({
         title: req.body.title,
         description: req.body.description,
         ingredients : req.body.ingredients,
             
     });
-        //handle later
-       //state ? req.body.state : null,
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+        //console.log(data)
+
+    }
+    catch (error) {
+        
+        res.status(400).json({message: error.message})
+    }
+})
+
+//Post Method
+router.post('/user', async (req, res) => {
+    const data = new User({
+        name : req.body.name,
+        restrictions : req.body.restrictions,
+        email : req.body.mail,
+            
+    });
 
     try {
         const dataToSave = await data.save();
