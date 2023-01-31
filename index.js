f70bf783771cf6c3b95a3e261f358ea8c3bb5437
Cooper <cooper@cooperstandard.org>
2023-01-30 17:23:51 -0700

    cors?

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ database.once('connected', () => {
 })
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json());
 
 //all endpoints start from /api
