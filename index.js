b0b15512bda15e1b5fc66a49490dc2ef21fcc4c7
Cooper <cooper@cooperstandard.org>
2023-02-15 16:23:36 -0700

    fix another cors issue

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ database.once('connected', () => {
 
 const app = express();
 
-//TODO: disables cors, want to fix later
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    //res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*/*");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
