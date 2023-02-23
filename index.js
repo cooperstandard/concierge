54ceee65593ff41f62e7391daab934336bb43ed6
Cooper <cooper@cooperstandard.org>
2023-02-22 17:24:56 -0700

    log url

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    console.log(req.url)
     next();
 });
 
