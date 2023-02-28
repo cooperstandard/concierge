33c79286a0564c52c451fb64234a9a78e7f51f01
Cooper <cooper@cooperstandard.org>
2023-02-28 13:01:35 -0700

    remove log string

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ Key is saved at:         /etc/letsencrypt/live/concierge.cooperstandard.org/priv
 
 mongoose.set('strictQuery', false);
 mongoose.connect(mongoString);
-console.log(mongoString)
+//console.log(mongoString)
 const database = mongoose.connection;
 
 database.on('error', (error) => {
